402da19c5bbfda9b6eb33724a83de33a143b806e
Asha Shiddo <ashashiddo@Ashas-MacBook-Air.local>
2023-04-23 18:26:48 -0400

    adding submissions to github

---
+++ test.js
@@ -0,0 +1,22 @@
+// //"Suzie is 32 years old."
+// const ageCalculator = function (name, yearOfBirth, currentYear) {
+//   let age = currentYear - yearOfBirth
+//   return name + " is " + age + " years old."
+// }
+// console.log(ageCalculator("Suzie", 1983, 2015));
+
+
+// const howManyHundreds = function (bottlesOfSyrup) {
+//  return Math.floor(bottlesOfSyrup/100 )
+// }
+// console.log(howManyHundreds(1000), "=?", 10);
+// console.log(howManyHundreds(894), "=?", 8);
+// console.log(howManyHundreds(520), "=?", 5);
+// console.log(howManyHundreds(99), "=?", 0);
+// console.log(howManyHundreds(0), "=?", 0);
+
+const calculateRectangleArea = function (length, width) {
+  
+}
+calculateTriangleArea(base, height)
+calculateCircleArea(radius)
